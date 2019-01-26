61da81d449e67640c59570ffba9637f9e5a0bd79
harvaaro <harvaaro@isu.edu>
2019-01-26 14:09:40 -0700

    Working on getting hte index of in SLL to output correctly

---
+++ src/main/java/edu/isu/cs/cs3308/SolitaireDecrypt.java
@@ -0,0 +1,10 @@
+package edu.isu.cs.cs3308;
+
+public class SolitaireDecrypt {
+	public SolitaireDecrypt(String deck) {
+	}
+
+	String execute(String encString) {
+		return encString;
+	}
+}
---
+++ src/main/java/edu/isu/cs/cs3308/SolitaireEncrypt.java
@@ -0,0 +1,10 @@
+package edu.isu.cs.cs3308;
+
+public class SolitaireEncrypt {
+	public SolitaireEncrypt(String deck) {
+	}
+
+	String execute(String decString) {
+		return decString;
+	}
+}
